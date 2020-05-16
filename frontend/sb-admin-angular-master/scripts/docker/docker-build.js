0da5dd153a851aafe9143a4bdc0154fb42e0ddc6
RansikaP <ransika.p27@gmail.com>
2020-05-16 01:38:45 -0400

    a bunch of stuff

--- frontend/sb-admin-angular-master/scripts/docker/docker-build.js
+++
@@ -1,7 +0,0 @@
-const version = require('../version.js');
-const sh = require('shelljs');
-
-const imageName = 'sb-admin-angular';
-
-sh.exec(`docker build -t ${imageName}:latest -t ${imageName}:${version} .`);
-
--- frontend/sb-admin-angular-master/src/modules/tables/services/index.ts
+++
@@ -1,7 +0,0 @@
-import { CountryService } from './country.service';
-import { TablesService } from './tables.service';
-
-export const services = [TablesService, CountryService];
-
-export * from './tables.service';
-export * from './country.service';
