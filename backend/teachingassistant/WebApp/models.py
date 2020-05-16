2be0353feba67b6b6fdfee598c4c20ead5ced6ba
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-05-16 00:50:51 -0400

    stuff

--- backend/teachingassistant/WebApp/models.py
+++ backend/teachingassistant/WebApp/models.py
@@ -1,5 +1,6 @@
 from djongo import models
 from django import forms
+import datetime
 # Create your models here.
 # class BadWord(models.Model):
 #     word = models.CharField(max_length=255)
@@ -64,8 +65,62 @@ class StringFieldForm(forms.ModelForm):
             'word', 'assigned_by'
         )
 
-class Day(models.Model):
-    x`
+class AStudent(models.Model):
+    name = models.CharField(max_length=100)
+    discord_name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+class AStudentForm(forms.ModelForm):
+    class Meta:
+        model = AStudent
+        fields = (
+            'name', 'discord_name'
+        )
+
+class AttendanceEntry(models.Model):
+    student = models.EmbeddedField(
+        model_container=AStudent,
+        model_form_class=AStudentForm
+    )
+    presence = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class AttendanceEntryForm(forms.ModelForm):
+    class Meta:
+        model = AttendanceEntry
+        fields = (
+            'student', 'presence'
+        )
+
+class Attendance(models.Model):
+    day = models.DateField(default=datetime.date.today)
+    student_list = models.ArrayField(
+        model_container=AttendanceEntry,
+        model_form_class=AttendanceEntryForm
+    )
+
+    class Meta:
+        abstract = True
+# class CurrentDay(models.Model):
+#     day = models.DateField()
+#     attend_list = models.EmbeddedField(
+#         model_container=Attendance,
+#         model_form_class=AttendanceForm
+#     )
+#
+#     class Meta:
+#         abstract = True
+
+class AttendanceForm(forms.ModelForm):
+    class Meta:
+        model = Attendance
+        fields = (
+            'day', 'student_list'
+        )
 
 class Classroom(models.Model):
     teacher = models.EmbeddedField(
@@ -86,6 +141,11 @@ class Classroom(models.Model):
     filter_words = models.ArrayField(
         model_container=StringField,
         model_form_class=StringFieldForm
+    )
+
+    attendance = models.ArrayField(
+        model_container=Attendance,
+        model_form_class=AttendanceForm
     )
 
 # class Words(models.Model):
