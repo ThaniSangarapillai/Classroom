e7c44cd89c9125de544fbc7307ab46414e7fe79f
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-05-16 23:43:43 -0400

    take two

--- frontend/sb-admin-angular-master/src/modules/dashboard/services/dashboard.service.ts
+++ frontend/sb-admin-angular-master/src/modules/dashboard/services/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+//import { DashboardTablesComponent } from '@modules/dashboard/components/dashboard-tables/dashboard-tables.component';
 import { Country } from '@modules/tables/models/tables.model';
 import { Observable, of } from 'rxjs';
 
@@ -10,28 +11,28 @@ export class DashboardService {
     readonly ROOT_URL = 'http://34.125.57.52/';
     pass = 'X7Mz&&am:&dOhnhk|Oq0$W^MYgkD3V|jgp/17{5=I4QLC:HFpC&P+FgL>Aw-F';
     user = 'TeachingAssistant';
-    table!: HTMLFormElement;
 
     getDashboard$(): Observable<{}> {
         return of({});
     }
 
     getData() {
-        const headerOptions = {
+        const headerOptions = { 
             // headers: new HttpHeaders({
             //     'Content-Type': 'application/json',
             //     'Authorization': 'Basic ' + btoa(this.user + ':' + this.pass),
             // })
         };
-        return this.http.post<any>(this.ROOT_URL + 'students/', {
+        this.http.post<any>(this.ROOT_URL + 'students/', {
             discord_name: 'Thani#4847',
             email: 'thanigajan@gmail.com',
+        }).subscribe(data => {
+            for (let x in data) {
+                this.posts.pipe()
+            }
         });
     }
-    hide_tables_store(table: HTMLFormElement) {
-        this.table = table;
-    }
-    hide_tables() {
-        this.table.hidden = true;
-    }
+    // hide_tables() {
+    //     this.tables.hide();
+    // }
 }
