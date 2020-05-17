ef4392fcaa8612c2b225693911460d3c774bc398
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-05-17 00:43:24 -0400

    changes!

--- frontend/takeTwo/teacher/src/app/app.module.ts
+++ frontend/takeTwo/teacher/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentTableComponent } from './student-table/student-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentTableComponent
   ],
   imports: [
     BrowserModule,
